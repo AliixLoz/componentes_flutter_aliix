5a54fe3ab2f567f2a8f5849dd4cf8a50655d0806
Aliix <loveitza50@gmail.com>
2024-02-21 22:16:26 -0600

    Adding inputs,infinite_scroll,notifications and data_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/screens/data_screen.dart';
+import 'package:practica3/screens/infinite_scroll_screen.dart';
+import 'package:practica3/screens/inputs_screen.dart';
+import 'package:practica3/screens/notifications_screen.dart';
 import 'package:practica3/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,48 +12,80 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Componentes de flutter'),
+        title: const Text('Componentes de Flutter'),
       ),
-        body: ListView(
-          children: <Widget>[
-              ListTile(
-                title: Text(
-                  'Entradas',
-                  style: AppTheme.lightTheme.textTheme.headlineLarge,
-                  ),
-                subtitle: Text(
-                  'Introduciendo valores',
-                  style: AppTheme.lightTheme.textTheme.bodySmall,
-                  ),
-                leading: const Icon(Icons.input_rounded),
-                trailing: const Icon(Icons.arrow_circle_right_rounded),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text(
+              'Entradas',
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+            ),
+            subtitle: Text(
+              'Introducción de valores',
+               style: AppTheme.lightTheme.textTheme.bodySmall,
               ),
-              Divider(),
-              ListTile(
-                title: Text(
-                  'Listview.builder',
-                  style: AppTheme.lightTheme.textTheme.headlineLarge
-                  ),
-                subtitle: Text('Lista con scroll infinito',
-                style: AppTheme.lightTheme.textTheme.bodySmall,
-                ),
-                leading: const Icon(Icons.format_list_bulleted_rounded),
-                trailing: const Icon(Icons.arrow_circle_right_rounded),
+            leading: const Icon(Icons.import_contacts_sharp),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
+            
+            //agreganuevo
+            onTap: () {
+              final ruta1 = MaterialPageRoute(builder: (context) {
+                return const InputsScreen(); 
+              });
+              Navigator.push(context,ruta1);
+            },
+            //acaba lo nuevo
+          
+          ),
+
+          const Divider(),
+          ListTile(
+            title: Text(
+              'ListView.builder',
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+            ),
+            subtitle: Text(
+              'Lista con desplazamiento infinito',
+               style: AppTheme.lightTheme.textTheme.bodySmall,
               ),
-              Divider(),
-              ListTile(
-                title: Text(
-                  'Notificaciones',
-                  style: AppTheme.lightTheme.textTheme.headlineLarge
-                  ),
-                subtitle: Text('Mostrar notis',
-                style: AppTheme.lightTheme.textTheme.bodySmall,
-                ),
-                leading: const Icon(Icons.notification_important_rounded),
-                trailing: const Icon(Icons.arrow_circle_right_rounded),
+            leading: const Icon(Icons.format_list_bulleted),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
+
+             //agreganuevo
+            onTap: () {
+              final ruta2 = MaterialPageRoute(builder: (context) {
+                return const InfiniteScrollScreen(); 
+              });
+              Navigator.push(context,ruta2);
+            },
+            //acaba lo nuevo
+          ),
+          const Divider(),
+
+          ListTile(
+            title: Text(
+              'Notificaciones',
+              style: AppTheme.lightTheme.textTheme.headlineLarge,
+            ),
+            subtitle: Text(
+              'Mostrar una notificación',
+               style: AppTheme.lightTheme.textTheme.bodySmall,
               ),
-            ],
-        ),  
+            leading: const Icon(Icons.notifications_active),
+            trailing: const Icon(Icons.arrow_circle_right_rounded),
+
+             //agreganuevo
+            onTap: () {
+              final ruta3 = MaterialPageRoute(builder: (context) {
+                return const NotificationsScreen(); 
+              });
+              Navigator.push(context,ruta3);
+            },
+            //acaba lo nuevo
+          ),
+        ],
+      ),
     );
   }
 }
