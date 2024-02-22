7116174771280596a262059a3822d4a1654550d0
Aliix <loveitza50@gmail.com>
2024-02-22 14:02:11 -0600

    Adding ElevatedButtons in InputsScreen

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,33 +2,37 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class AppTheme {
+class AppTheme{
   //constante de un color primario
-  static const primaryColor = Color.fromARGB(223, 221, 63, 63);
-  //Constante de color secundario
-  static const secondaryColor = Color.fromARGB(255, 95, 50, 50);
+  static const primaryColor = Color.fromARGB(223, 174, 29, 92);
+  //constante de un color secundario
+  static const secondaryColor = Color.fromARGB(255, 188, 159, 15); 
   //constante de color de fondo
-  static const backColor = Color.fromARGB(223, 235, 246, 247);
+  static const backColor = Color.fromARGB(227, 165, 206, 209);
   //constante de tema
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     scaffoldBackgroundColor: backColor,
-    appBarTheme: const AppBarTheme(
-      color: primaryColor,
-    ),
-    textTheme: TextTheme(
-      //Títulos muy grandes
-      headlineLarge: GoogleFonts.abrilFatface(
-        color: primaryColor,
-        fontSize: 26.5,
-        fontWeight: FontWeight.w500,
-        fontStyle: FontStyle.italic,
-        decoration: TextDecoration.underline,
-        decorationColor: primaryColor,
-        decorationStyle: TextDecorationStyle.wavy,
-        decorationThickness: 1.5,
+        appBarTheme: const AppBarTheme(
+          color: primaryColor,
       ),
-      //Estilo para texto pequeño
-
-    ),
+      textTheme: TextTheme(
+        //Titulos muy grandes
+        headlineLarge: GoogleFonts.dancingScript(
+          color: primaryColor,
+          fontSize: 26.5,
+          fontWeight: FontWeight.w500,
+          fontStyle: FontStyle.italic,
+          decoration: TextDecoration.underline,
+          decorationColor: primaryColor,
+          decorationStyle: TextDecorationStyle.dotted,
+          decorationThickness: 2.0,
+        ),
+        //Estilo para texto muy pequeño
+        bodySmall: GoogleFonts.shadowsIntoLight(
+          color: secondaryColor,
+          fontSize: 16.0,
+        ),
+      ),
   );
+
 }
