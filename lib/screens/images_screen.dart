cdbb7c65ccc5a9853c9c648c3e1e3743efebebf2
Aliix <loveitza50@gmail.com>
2024-03-05 12:01:55 -0600

    Images

--- lib/screens/images_screen.dart
+++ lib/screens/images_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:practica3/theme/app_theme.dart';
+import 'package:transparent_image/transparent_image.dart';
+
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -14,12 +17,60 @@ class _ImageScreenState extends State<ImageScreen> {
         appBar: AppBar(
           title: const Text('Imagenes'),
         ),
-        body: const Column(
+        body: ListView(
           children: [
-            Image(
-              image: AssetImage('assets/img/f1.png'),
-            )
+            cardImage1(),
+            imageFade(),
           ],
         ));
+  }
+
+  Card cardImage1() {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25),
+      ),
+      margin: const EdgeInsets.all(15),
+      elevation: 10,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(18),
+        child: Column(
+          children: [
+            const Image(
+              image: AssetImage('assets/img/f1.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'F1',
+                style: AppTheme.lightTheme.textTheme.headlineMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SizedBox image2(){
+    return SizedBox(
+      height: 200.0,
+      width: 200.0,
+      child: Image.network('https://i.pinimg.com/564x/b7/4e/3a/b74e3aaeaf708661dc60796ffa678139.jpg'),
+    );
+  }
+  Stack imageFade(){
+    return Stack(
+      children: <Widget>[
+        const Center(
+          child: CircularProgressIndicator(),
+        ),
+        Center( 
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage, 
+            image: 'https://i.pinimg.com/564x/b7/4e/3a/b74e3aaeaf708661dc60796ffa678139.jpg'),
+        ),
+      ],
+    );
   }
 }
